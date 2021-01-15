fc924efc612cb89224bd0e1bb25fe3f6a4aaab58
Tatejimaru <tatejimaru137@gmail.com>
2021-01-15 21:12:41 +0900

    [mod]ダークモード関連処理の見直し

--- src/content.js
+++ src/content.js
@@ -1,43 +1,38 @@
 'use strict';
 
-const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-if (isDark)
-{
-    const body = document.querySelector('body');
-    const fadeInLayer = document.createElement('div');
+const html = document.querySelector('html');
+const body = document.querySelector('body');
+const fadeInLayer = document.createElement('div');
 
-    fadeInLayer.id = 'fade-in-layer';
-    body.style.opacity = '100%';
-    document.querySelector('html').style.backgroundColor = 'transparent';
+fadeInLayer.id = 'fade-in-layer';
+body.style.opacity = '100%';
+html.style.backgroundColor = 'transparent';
+
+fadeInLayer.dataset.fadein = 'false';
+body.appendChild(fadeInLayer);
+
+setTimeout(() =>
+{
+    fadeInLayer.dataset.fadein = 'true';
+}, 100);
+
+
+chrome.runtime.onMessage.addListener((message) =>
+{
+    if (message.message !== 'activated')
+    {
+        return true;
+    }
+
+    if (body.contains(fadeInLayer))
+    {
+        body.removeChild(fadeInLayer);
+    }
 
     fadeInLayer.dataset.fadein = 'false';
     body.appendChild(fadeInLayer);
-
-
     setTimeout(() =>
     {
         fadeInLayer.dataset.fadein = 'true';
-    }, 500);
-
-    chrome.runtime.onMessage.addListener((message) =>
-    {
-        if (message.message !== 'activated')
-        {
-            return true;
-        }
-
-        if (body.contains(fadeInLayer))
-        {
-            body.removeChild(fadeInLayer);
-        }
-
-        fadeInLayer.dataset.fadein = 'false';
-        body.appendChild(fadeInLayer);
-        setTimeout(() =>
-        {
-            fadeInLayer.dataset.fadein = 'true';
-        }, 500);
-        return true;
-    });
-}
-
+    }, 100);
+});
